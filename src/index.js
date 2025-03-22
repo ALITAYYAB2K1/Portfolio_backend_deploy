a3bad99329b21045c3b286c5a2d8b2b82068a134
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-22 18:12:43 +0500

    dd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import connectDB from "./db/index.js";
 import app from "./app.js";
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT || 8000}`);
     });
   })
